a9fa0d4c005bde4fabbf9f0e1044ad03f0c43f76
amir73911 <gafurovamir@gmail.com>
2014-01-24 18:02:06 +0400

    24,01

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
 $(function(){
-    var rsi = $('.slider').royalSlider({
+    $('#slider').royalSlider({
         autoHeight: false,
         arrowsNav: false,
         fadeinLoadedSlide: false,
@@ -14,18 +14,35 @@ $(function(){
         autoScaleSlider: true,
         autoScaleSliderWidth: 980,
         autoScaleSliderHeight: 422,
-
-        /* size of all images http://help.dimsemenov.com/kb/royalslider-jquery-plugin-faq/adding-width-and-height-properties-to-images */
         imgWidth: 980,
         imgHeight: 422
-
-
-
-    }).data('royalSlider');
-    $('#slider-next').click(function() {
-        rsi.next();
     });
-    $('#slider-prev').click(function() {
-        rsi.prev();
+
+    $('#slider2').royalSlider({
+        controlNavigation: 'thumbnails',
+        thumbs: {
+            orientation: 'vertical',
+            paddingBottom: 4,
+            appendSpan: true
+        },
+        transitionType:'fade',
+        autoScaleSlider: true,
+        autoScaleSliderWidth: 960,
+        autoScaleSliderHeight: 600,
+        loop: true,
+        arrowsNav: false,
+        keyboardNavEnabled: true
+
     });
+
+    // оформление топ_меню
+    if ($('.top_nav .active').length) {
+        $('.top_nav').addClass('have-active');
+    };
+
+    // опшены в слайдере2
+    $('.slider2_back .options a').click(function(){
+        $(this).toggleClass('selected');
+    });
+
 });
